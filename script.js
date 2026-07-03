460ecb85a10e7bb01240592636d6b72411a6f62b
acaukorea152 <acaukorea152@gmail.com>
2026-07-03 13:51:44 +0900

    First website

---
+++ script.js
@@ -0,0 +1,15 @@
+const ctaBtn=document.getElementById("ctaBtn");
+
+ctaBtn.addEventListener("click",()=>{
+
+alert("무료 체험 신청 페이지로 이동!");
+
+});
+
+const submitBtn=document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click",()=>{
+
+alert("신청이 완료되었습니다!");
+
+});
